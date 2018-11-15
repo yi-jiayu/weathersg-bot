eb5087f3b7a533bfecdba907825b784acafb98af
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-11-15 16:27:29 +0800

    Add hello world handler

--- project.clj
+++ project.clj
@@ -2,8 +2,12 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-json "0.4.0"]]
+  :java-source-paths ["src/java"]
   :main ^:skip-aot weathersg-bot.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/weathersg_bot/core.clj
+++ src/weathersg_bot/core.clj
@@ -1,7 +1,14 @@
 (ns weathersg-bot.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.json :refer [wrap-json-body]])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn handler [request]
+  (println (:body request))
+  {:status  200
+   :headers {"Content-Type" "text/plain"}
+   :body    "Hello, World"})
+
+(defn -main [& args]
+  (run-jetty (wrap-json-body handler)
+             {:port (Integer/valueOf ^String (or (System/getenv "PORT") "3000"))}))
