42f51543f2ec750c812b623e480eb4f2562e9a7f
Jiayu Yi <jiayu.yi@thoughtworks.com>
2018-11-16 16:16:54 +0800

    Refactor rain_areas.clj

--- src/weathersg_bot/rain_areas.clj
+++ src/weathersg_bot/rain_areas.clj
@@ -3,21 +3,12 @@
             [clojure.java.shell :refer [sh]])
   (:import (java.time ZoneId ZonedDateTime)
            (java.time.format DateTimeFormatter)
-           (java.nio.file Files Path LinkOption Paths)
-           (java.nio.file.attribute FileAttribute)
-           (java.io FileNotFoundException)))
+           (java.nio.file Paths)
+           (java.io FileNotFoundException File)))
 
 (def ^ZoneId singapore-zone (ZoneId/of "Asia/Singapore"))
 (def radar-timestamp-formatter (DateTimeFormatter/ofPattern "yyyyMMddHHmm0000"))
 (def radar-overlay-prefix "https://www.weather.gov.sg/files/rainarea/50km/v2/")
-(def data-dir (Paths/get "data" (make-array String 0)))
-(def base-img (.resolve data-dir "base.png"))
-(def overlays-dir (.resolve data-dir "overlays"))
-(def ^Path rain-areas-dir (.resolve data-dir "rain-areas"))
-
-(Files/createDirectories overlays-dir (make-array FileAttribute 0))
-(Files/createDirectories rain-areas-dir (make-array FileAttribute 0))
-(io/copy (io/input-stream (io/resource "base.png")) (.toFile base-img))
 
 (defn truncate-timestamp
   "Returns a timestamp rounded down to the closest multiple of 5 minutes."
@@ -28,64 +19,62 @@
         truncated-ts (.minusMinutes ts delta-minutes)]
     truncated-ts))
 
-(defn get-key
+(defn get-formatted-timestamp
   [ts]
   (.format radar-timestamp-formatter (truncate-timestamp ts)))
 
 (defn get-radar-overlay-name
-  [key]
-  (str "dpsri_70km_" key "dBR.dpsri.png"))
+  [formatted-ts]
+  (str "dpsri_70km_" formatted-ts "dBR.dpsri.png"))
 
 (defn get-radar-overlay-url
-  [key]
-  (str radar-overlay-prefix (get-radar-overlay-name key)))
+  [formatted-ts]
+  (str radar-overlay-prefix (get-radar-overlay-name formatted-ts)))
 
-(defn get-current-time
+(defn get-current-sg-time
   []
   (ZonedDateTime/now singapore-zone))
 
-(defn exists?
-  [^Path path]
-  (Files/exists path (make-array LinkOption 0)))
-
 (defn download-radar-overlay
-  ([url dest]
-   (do (let [in (io/input-stream url)
-             out (.toFile dest)]
-         (io/copy in out))
-       dest))
-  ([^String key]
-   (let [overlay-url (get-radar-overlay-url key)
-         overlay-path (.resolve overlays-dir key)]
-     (do (if (not (exists? overlay-path))
-           (download-radar-overlay overlay-url overlay-path))
-         (str overlay-path)))))
+  "Downloads a radar overlay from `url` to `dst`."
+  [url dst]
+  (with-open [in (io/input-stream url)
+              out (io/output-stream dst)]
+    (io/copy in out)))
+
+(defn generate-rain-areas
+  [overlay-path output-path]
+  (with-open [base-img (io/input-stream (io/resource "base.png"))]
+    (sh "magick"
+        "composite"
+        overlay-path
+        "png:-"
+        "-geometry" "853x479!"
+        "-blend" "50"
+        output-path
+        :in base-img)))
 
 (defn get-rain-areas
-  ([base overlay output]
-   (do (sh "magick"
-           "composite"
-           overlay
-           base
-           "-geometry" "853x479!"
-           "-blend" "50"
-           output)
-       output))
-  ([^String key]
-   (let [output-path (.resolve rain-areas-dir key)]
-     (do (if (not (exists? output-path))
-           (let [overlay-path (download-radar-overlay key)
-                 output-path (str output-path)]
-             (get-rain-areas (str base-img) overlay-path output-path)))
-         (str output-path)))))
+  [formatted-ts output-path]
+  (let [radar-overlay-url (get-radar-overlay-url formatted-ts)
+        temp-file (File/createTempFile "radar" nil)
+        overlay-path (.getPath temp-file)]
+    (do (download-radar-overlay radar-overlay-url temp-file)
+        (generate-rain-areas overlay-path output-path)
+        (.delete temp-file))))
 
 (defn get-latest-rain-areas
-  []
-  (loop [ts (get-current-time)]
-    (let [rain-areas (try
-                       (get-rain-areas (get-key ts))
-                       (catch FileNotFoundException _
-                         nil))]
-      (if (nil? rain-areas)
-        (recur (.minusMinutes ts 5))
-        rain-areas))))
+  [rain-areas-dir]
+  (loop [ts (get-current-sg-time)]
+    (let [formatted-ts (get-formatted-timestamp ts)
+          output-path (str (Paths/get rain-areas-dir (into-array [(str formatted-ts ".png")])))]
+      (if (.exists (io/file output-path))
+        output-path
+        (let [success? (try
+                         (do (get-rain-areas formatted-ts output-path)
+                             true)
+                         (catch FileNotFoundException _
+                           false))]
+          (if success?
+            output-path
+            (recur (.minusMinutes ts 5))))))))
--- test/weathersg_bot/rain_areas_test.clj
+++ test/weathersg_bot/rain_areas_test.clj
@@ -11,6 +11,15 @@
   (testing "should truncate minutes to closest multiple of 5"
     (is (= reference-date-time (truncate-timestamp (.plusMinutes reference-date-time 3))))))
 
+(deftest get-formatted-timestamp-test
+  (is (= "2018111508450000" (get-formatted-timestamp reference-date-time))))
+
 (deftest get-radar-overlay-name-test
   (is (= "dpsri_70km_2018111508450000dBR.dpsri.png"
-         (get-radar-overlay-name (truncate-timestamp reference-date-time)))))
+         (get-radar-overlay-name (get-formatted-timestamp reference-date-time)))))
+
+(deftest get-radar-overlay-url-test
+  (is (= "https://www.weather.gov.sg/files/rainarea/50km/v2/dpsri_70km_2018111508450000dBR.dpsri.png"
+         (get-radar-overlay-url (get-formatted-timestamp reference-date-time)))))
+
+;; todo: test remaining functions with mocks
